512d3bd29cdd912d2e139ea9f07610beee1ad778
Katarzyna Strzelinska <katarzyna.strzelinska@op.pl>
2024-09-21 17:40:14 +0200

    add capitalize

--- zad1.py
+++ zad1.py
@@ -5,4 +5,6 @@ shopping_list = {
 
 print("Lista zakupów")
 for i, j in shopping_list.items():
-    print(f"Idę do {i} i kupuję tam: {j}")
+    # Capitalize the store name and each item in the list
+    capitalized_items = [item.capitalize() for item in j]
+    print(f"Idę do {i.capitalize()} i kupuję tam: {', '.join(capitalized_items)}")
