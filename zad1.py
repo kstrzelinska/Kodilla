4416c34f0bb0e87e5010bc95798d31226b9d8a45
Katarzyna Strzelinska <katarzyna.strzelinska@op.pl>
2024-09-21 17:39:18 +0200

    add loop

--- zad1.py
+++ zad1.py
@@ -2,3 +2,7 @@ shopping_list = {
     'piekarnia': ['chleb', 'pączek', 'bułki'],
     'warzywniak': ['marchew', 'seler', 'rukola']
 }
+
+print("Lista zakupów")
+for i, j in shopping_list.items():
+    print(f"Idę do {i} i kupuję tam: {j}")
